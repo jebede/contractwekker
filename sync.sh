e48d8421f78468ac395837b90b183b4b01539ec4
Jelle <jelle@jebede.nl>
2025-08-28 16:27:30 +0200

    minor change + adding simple testing

--- sync.sh
+++ sync.sh
@@ -3,8 +3,5 @@
 cd "$(dirname "$0")" || exit 1
 
 git pull origin main
-composer install --no-interaction --no-dev --prefer-dist
-php artisan migrate --force
-#php artisan route:cache
-#php artisan config:cache
-#php artisan responsecache:clear
+#composer install --no-interaction --no-dev --prefer-dist
+# migrate.php ...
